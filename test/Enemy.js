37b8f5ff8d650109c1680965433a212a485e255d
charredlot <charlotte.sp.tan@gmail.com>
2018-09-09 23:53:07 -0700

    set collide world bounds for enemies
    needs to be set on container body, not sprite

--- test/Enemy.js
+++ test/Enemy.js
@@ -34,14 +34,13 @@ Enemy.prototype.createCommon = function(scene, container, sprite) {
     
     let boundingBox = sprite.getBounds();
     container.body.setSize(boundingBox.width, boundingBox.height);
-    
+    container.body.setCollideWorldBounds(true);
     /*
      * immovable in physics.add.group makes physics
      * interactions fail completely, this seems to do what
      * we want. alternatively may want to set mass to infinity
      */
     sprite.body.moves = false;
-    sprite.setCollideWorldBounds(true);
 
     container.__enemy = this;
     sprite.__enemy = this;
